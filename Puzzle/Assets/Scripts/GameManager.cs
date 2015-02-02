f4d07b9543dfc10d4796dac76118e642eb37acab
R <macuser@MacUser-no-MacBook-Air.local>
2015-02-03 02:15:02 +0900

    ゲームの状態設定

---
+++ Puzzle/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	enum State{
+		WAIT,
+		PUZZLE,
+		COMBO,
+		ATTACK,
+		ENEMYATTACK,
+		CLEAR,
+		GAMEOVER
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
