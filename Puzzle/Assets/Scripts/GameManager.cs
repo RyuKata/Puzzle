775200462807d6cc41d5878bcf67bfa15e74bed6
R <macuser@MacUser-no-MacBook-Air.local>
2015-02-04 02:03:33 +0900

    タップしたドロップを半透明に

--- Puzzle/Assets/Scripts/GameManager.cs
+++ Puzzle/Assets/Scripts/GameManager.cs
@@ -15,7 +15,9 @@ public class GameManager : MonoBehaviour {
 
 	int operate = 4;
 	State state;
-	GameObject tapObj;
+	GameObject tapDrop;
+	GameObject moveDrop;
+	Color dropColor;
 
 	// Use this for initialization
 	void Start () {
@@ -34,21 +36,28 @@ public class GameManager : MonoBehaviour {
 				Collider2D col = Physics2D.OverlapPoint (tapPoint);
 
 				if (col) {
-					tapObj = col.transform.gameObject;
+					tapDrop = col.transform.gameObject;
+					dropColor = tapDrop.renderer.material.color;
+					tapDrop.renderer.material.color = new Color(dropColor.r,dropColor.g,dropColor.b,0.5f);
+					moveDrop = Object.Instantiate(col.transform.gameObject) as GameObject;
 				}
 			}
 
 			//Invoke ("OperationTime", operate);
 			if (Input.GetMouseButton (0)) {
 				Vector2 tapPoint = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-				if (tapObj != null) {
-					tapObj.transform.position = tapPoint;
+				if (tapDrop != null) {
+					moveDrop.transform.position = tapPoint;
 				}
 			}
 
 			if (Input.GetMouseButtonUp (0)) {
-				tapObj = null;
+				if (tapDrop != null) {
+					tapDrop.renderer.material.color = dropColor;
+					tapDrop = null;
+					Destroy (moveDrop);
 //				state = State.COMBO;
+				}
 			}
 
 			break;
